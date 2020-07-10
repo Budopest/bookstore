68a3c8cca9853f01f7398cd731c30ac52c76b49a
budopest <abdo.mohamed5111995@gmail.com>
2020-07-10 13:31:50 +0200

    put initialization code in init() method

--- src/test/java/bookstoread/BookShelfSpec.java
+++ src/test/java/bookstoread/BookShelfSpec.java
@@ -1,5 +1,7 @@
 package bookstoread;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -7,15 +9,20 @@ import java.util.List;
 
 public class BookShelfSpec {
 
+    private BookShelf shelf;
+    @BeforeEach
+    public void init() throws Exception{
+        shelf = new BookShelf();
+    }
+
     @Test
-    public void shelfEmptyWhenNoBookAdded() throws Exception{
+    public void shelfEmptyWhenNoBookAdded(){
         /*
         * Test case is broken into three parts :
         * We set up data that our test case needs.
         * We call the unit being tested.
         * We perform assertions to verify if expected behavior is correct.
         * */
-        BookShelf shelf = new BookShelf();
         List<String> books = shelf.getAllBooksInShelf();
         assertTrue(books.isEmpty(),()->"BookShelf should be empty");
 
@@ -23,8 +30,6 @@ public class BookShelfSpec {
 
     @Test
     public void bookShelfContainsThreeBooksWhenThreeBooksAdded(){
-
-        BookShelf shelf = new BookShelf();
         shelf.addBooksToShelf("JAVA the complete refrence");
         shelf.addBooksToShelf("JAVA unit testing with Junit5","MYSQL Fundementals");
         List<String> books = shelf.getAllBooksInShelf();
@@ -34,8 +39,6 @@ public class BookShelfSpec {
 
     @Test
     public void bookShelfEmptyWhenNoBokksAdded(){
-
-        BookShelf shelf = new BookShelf();
         List<String> books = shelf.getAllBooksInShelf();
         assertTrue(books.isEmpty(),()-> "Bookshelf should be empty");
 
@@ -43,7 +46,6 @@ public class BookShelfSpec {
 
     @Test
     public void booksListunmodifiable(){
-        BookShelf shelf = new BookShelf();
         List<String> books = shelf.getAllBooksInShelf();
         try{
         books.add("12 Rules for life");
