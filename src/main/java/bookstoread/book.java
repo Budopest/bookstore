7dd508e8c85b1e49e4f13f9b07074cbf381d82b2
budopest <abdo.mohamed5111995@gmail.com>
2020-07-10 14:19:50 +0200

    Add book class

---
+++ src/main/java/bookstoread/book.java
@@ -0,0 +1,37 @@
+package bookstoread;
+
+import java.time.LocalDate;
+
+public class book {
+    private final String title;
+    private final String author;
+    private final LocalDate publishedOn;
+
+    public book(String title, String author, LocalDate publishedOn) {
+        this.title = title;
+        this.author = author;
+        this.publishedOn = publishedOn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public LocalDate getPublishedOn() {
+        return publishedOn;
+    }
+
+    @Override
+    public String toString() {
+        // ctrl + alt + l for auto indention
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", publishedOn=" + publishedOn +
+                '}';
+    }
+}
